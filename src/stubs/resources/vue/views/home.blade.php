94c42ce111b621c223325b5b270d2e3cac2006df
Mosab Ibrahim <miaababikir@gmail.com>
2020-05-27 22:43:01 +0200

    Updation Vue home page

--- src/stubs/resources/vue/views/home.blade.php
+++ src/stubs/resources/vue/views/home.blade.php
@@ -1,9 +1,217 @@
-@extends('layouts.app')
+@extends('vue.layouts.app')
 
 @section('header')
     <base-header title="Home"></base-header>
 @endsection
 
 @section('content')
+
+    <div class="mx-auto w-full">
+        <div>
+            <!-- Card stats -->
+            <div class="flex flex-wrap -mx-4">
+                <div class="w-full md:w-1/3 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                        <div class="flex-auto p-4">
+                            <div class="flex flex-wrap">
+                                <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                    <h5 class="text-gray-500 uppercase font-bold text-xs">
+                                        New users
+                                    </h5>
+                                    <span class="font-semibold text-xl text-gray-800">
+                          2,356
+                        </span>
+                                </div>
+                                <div class="relative w-auto px-2 flex-initial">
+                                    <div
+                                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-orange-500">
+                                        <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                             stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path
+                                                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="text-sm text-gray-500 mt-4">
+                      <span class="text-red-500 mr-2">
+                        <i class="fas fa-arrow-down"></i> 3.48%
+                      </span>
+                                <span class="whitespace-no-wrap">
+                        Since last week
+                      </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/3 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                        <div class="flex-auto p-4">
+                            <div class="flex flex-wrap">
+                                <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                    <h5 class="text-gray-500 uppercase font-bold text-xs">
+                                        Sales
+                                    </h5>
+                                    <span class="font-semibold text-xl text-gray-800">
+                          924
+                        </span>
+                                </div>
+                                <div class="relative w-auto px-2 flex-initial">
+                                    <div
+                                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-pink-500">
+                                        <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                             stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path
+                                                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="text-sm text-gray-500 mt-4">
+                      <span class="text-orange-500 mr-2">
+                        <i class="fas fa-arrow-down"></i> 1.10%
+                      </span>
+                                <span class="whitespace-no-wrap">
+                        Since yesterday
+                      </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/3 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                        <div class="flex-auto p-4">
+                            <div class="flex flex-wrap">
+                                <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                    <h5 class="text-gray-500 uppercase font-bold text-xs">
+                                        Performance
+                                    </h5>
+                                    <span class="font-semibold text-xl text-gray-800">
+                          49,65%
+                        </span>
+                                </div>
+                                <div class="relative w-auto px-2 flex-initial">
+                                    <div
+                                        class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-blue-500">
+                                        <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                             stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path
+                                                d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="text-sm text-gray-500 mt-4">
+                      <span class="text-green-500 mr-2">
+                        <i class="fas fa-arrow-up"></i> 12%
+                      </span>
+                                <span class="whitespace-no-wrap">
+                        Since last month
+                      </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="mt-12">
+        <h2 class="text-2xl font-medium">Tables</h2>
+        <div class="mt-4">
+            <div class="flex flex-col">
+                <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6">
+                    <div
+                        class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                        <table class="min-w-full">
+                            <thead class="bg-gray-100">
+                            <tr>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-right text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                    style="text-align: start">
+                                    Name
+                                </th>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                    style="text-align: start">
+                                    Title
+                                </th>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                    style="text-align: start">
+                                    Status
+                                </th>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                    style="text-align: start">
+                                    Role
+                                </th>
+                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                            </tr>
+                            </thead>
+                            <tbody class="bg-white">
+                            <tr>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 h-10 w-10">
+                                            <img class="h-10 w-10 rounded-full"
+                                                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                 alt=""/>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="text-sm leading-5 font-medium text-gray-900">Jone Doe</div>
+                                            <div class="text-sm leading-5 text-gray-500">bernardlane@example.com</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                    Software Engineer
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                  <span
+                                      class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                    Active
+                                  </span>
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                    Owner
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 h-10 w-10">
+                                            <img class="h-10 w-10 rounded-full"
+                                                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                 alt=""/>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="text-sm leading-5 font-medium text-gray-900">Jone Doe</div>
+                                            <div class="text-sm leading-5 text-gray-500">bernardlane@example.com</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                    Actor
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                  <span
+                                      class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                    Active
+                                  </span>
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                    Owner
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
