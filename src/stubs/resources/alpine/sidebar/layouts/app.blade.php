59b554696d2b7b1e43e67f1ab2b67daf6bdb5f17
Mosab Ibrahim <miaababikir@gmail.com>
2020-06-02 20:24:38 +0200

    Changing logos

--- src/stubs/resources/alpine/sidebar/layouts/app.blade.php
+++ src/stubs/resources/alpine/sidebar/layouts/app.blade.php
@@ -9,15 +9,14 @@
     <script type="module" src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
     <script nomodule src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine-ie11.min.js" defer></script>
 
-    <script src="{{ mix('js/app.js') }}" defer></script>
-
+        <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 <body class="bg-gray-200 min-h-screen font-base">
 <div id="app">
 
     <div class="flex flex-col md:flex-row">
 
-        @include('includes.navbar')
+        @include('includes.sidebar')
 
         <div class="w-full md:flex-1">
             <nav class="hidden md:flex justify-between items-center bg-white p-4 shadow-md h-16">
@@ -31,7 +30,7 @@
                         <svg class="h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                              viewBox="0 0 24 24" stroke="currentColor">
                             <path
-                                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
+                                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path>
                         </svg>
                     </button>
                 </div>
--- src/stubs/resources/alpine/sidebar/layouts/auth.blade.php
+++ src/stubs/resources/alpine/sidebar/layouts/auth.blade.php
@@ -7,7 +7,6 @@
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}" defer></script>
-
 </head>
 <body class="bg-gray-200 min-h-screen font-base">
 <div id="app">
