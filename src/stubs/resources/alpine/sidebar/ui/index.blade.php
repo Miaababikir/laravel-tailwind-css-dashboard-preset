2895aa94a5d985e048fa81355d7a0223883cb45c
Mosab Ibrahim <miaababikir@gmail.com>
2020-06-06 23:13:12 +0200

    Removing header from alpine sidebar

--- src/stubs/resources/alpine/sidebar/ui/index.blade.php
+++ src/stubs/resources/alpine/sidebar/ui/index.blade.php
@@ -1,13 +1,5 @@
 @extends('layouts.app')
 
-@section('header')
-    <header class="bg-white shadow">
-        <div class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold leading-tight text-gray-900">Home</h1>
-        </div>
-    </header>
-@endsection
-
 @section('content')
 
     <div class="bg-white p-6 rounded shadow">
