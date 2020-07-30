80a2e2535e61a6e5561dc4b59190812877574380
Mosab Ibrahim <miaababikir@gmail.com>
2020-07-30 17:12:13 +0200

    Fixing some bugs

--- src/stubs/resources/alpine/stacked/layouts/auth.blade.php
+++ src/stubs/resources/alpine/stacked/layouts/auth.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Laravel</title>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
